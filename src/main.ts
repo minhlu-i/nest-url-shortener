c5380bf11d2d489d9ac42693745395d597769c8c
minhlu-i <quangminhphamlu@gmail.com>
2024-01-31 14:54:27 +0700

    update: lint rule & swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("Shortener API")
+    .setVersion("0.0.0")
+    .addTag("cats")
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("api", app, swaggerDocument);
+
   await app.listen(3000);
 }
+
 bootstrap();
